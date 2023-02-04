efc18a91bba63ffdb757505fede3c6749af5f530
Adrian Nawalinski <adrian.nawalinski@gmail.com>
2023-02-04 15:13:06 +0100

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyB7Who5tvpy-xlC1euXIhLH6GY7gpz_Tiw",
+    authDomain: "firestorecrud-d4991.firebaseapp.com",
+    databaseURL: "https://firestorecrud-d4991-default-rtdb.firebaseio.com",
+    projectId: "firestorecrud-d4991",
+    storageBucket: "firestorecrud-d4991.appspot.com",
+    messagingSenderId: "140081216115",
+    appId: "1:140081216115:web:5d39d21ee1c62c6edb0806"
+  }
 };
